6f4a8f3de6b3a52447102ec42d58ee4d5449bb1a
MathiasSchjoedt-Bavngaard <msb@iterator-it.dk>
2023-12-13 13:42:31 +0100

    added Reactive form and style

--- q04/src/app/app.module.ts
+++ q04/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 import { ReactiveComponent } from './reactive/reactive.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TemplateDrivenComponent,
-    ReactiveComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TemplateDrivenComponent, ReactiveComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
