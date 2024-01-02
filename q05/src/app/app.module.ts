6d8a45726ff9ec1df4c071fd29a295948f57e30f
Ninjaneer <simhjeppe@gmail.com>
2024-01-02 10:46:41 +0100

    q05 material, tailwind and basic components added

--- q05/src/app/app.module.ts
+++ q05/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BasicComponent } from './basic/basic.component';
+import { MaterialComponent } from './material/material.component';
+import { TailwindComponent } from './tailwind/tailwind.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicComponent,
+    MaterialComponent,
+    TailwindComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
